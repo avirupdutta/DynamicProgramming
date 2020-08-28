b91fcb5c762f334c6cbce23e4cfbe6d6ed2c7371
avirupdutta <finalguider99@gmail.com>
2020-08-28 20:50:47 +0530

    added the program for knapsack count subset with given diff problem

--- src/CountSubsetWithDiff.java
+++ src/CountSubsetWithDiff.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 
-public class CountSubsetSum {
+public class CountSubsetWithDiff {
 
     public static int countSubsetSum(int[] arr, int sum) {
         int[][] table = new int[arr.length+1][sum+1];
-
-//        initializing the table
+        
+//        table initialization
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < sum+1; j++) {
                 table[i][j] = 0;
@@ -13,30 +13,39 @@ public class CountSubsetSum {
             table[i][0] = 1;
         }
 
-//        main dp algorithm
+
         for (int i = 1; i < table.length; i++) {
             for (int j = 0; j < sum+1; j++) {
-
-                if (arr[i-1] <= j){
-                    table[i][j] = table[i-1][j] + table[i-1][j-arr[i-1]];
+                if (arr[i-1] <= j) {
+                    table[i][j] = table[i-1][j] + table[i-1][j - arr[i-1]];
                 }
                 else {
                     table[i][j] = table[i-1][j];
                 }
-
             }
         }
 
 //        for (int[] ints : table) {
 //            System.out.println(Arrays.toString(ints));
 //        }
-
+        
         return table[arr.length][sum];
     }
+    
+
+    public static int countSubsetWithDiff(int[] arr, int diff) {
+        int arrSum = 0;
+        for (int num : arr) {
+            arrSum += num;
+        }
+        int sum = (arrSum + diff) / 2;
+        return countSubsetSum(arr, sum);
+    }
+
 
     public static void main ( String[] args ) {
-        int[] arr = {2, 5, 3};
-        int sum = 5;
-        System.out.println("Is Available: "+countSubsetSum(arr, sum));
+        int[] arr = {1, 1, 2, 3};
+        int diff = 1;
+        System.out.println("Is Available: "+countSubsetWithDiff(arr, diff));
     }
 }
